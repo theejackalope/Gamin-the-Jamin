b5aaf4c8ad97ffd7f10e5da58c29f4d8b6f28e8c
Tyler Heslin <264850546+tylers-gamehub@users.noreply.github.com>
2026-03-02 21:40:07 -0700

    pause menu works

--- Godot/gamin-the-jamin/scripts/pause_menu.gd
+++ Godot/gamin-the-jamin/scripts/pause_menu.gd
@@ -14,11 +14,3 @@ func _on_resume_button_pressed():
 func _on_quit_button_pressed():
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/Menus/main_menu.tscn")
-
-
-func _on_resume_pressed() -> void:
-	pass # Replace with function body.
-
-
-func _on_quit_pressed() -> void:
-	pass # Replace with function body.
